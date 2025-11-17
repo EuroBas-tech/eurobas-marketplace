5c07237f78ed42e077b19a0bc05d6f6c0910f321
EuroBas-tech <eurobas10@gmail.com>
2025-11-18 00:35:53 +0100

    Add Dutch SEO language file for EuroBas

---
+++ resources/lang/nl/Seo.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'meta_title' => 'EuroBas – Europa’s uniforme marktplaats voor voertuigen & vastgoed',
+    'meta_description' => 'EuroBas is Europa’s gratis, uniforme marktplaats voor het kopen en verkopen van voertuigen, vastgoed, jachten, elektronica en vele andere producten. Maak een account aan, plaats onbeperkt gratis advertenties en kom rechtstreeks in contact met kopers en verkopers in heel Europa.',
+];
