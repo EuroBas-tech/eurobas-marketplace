7c8cd0e7fbd249a605436ce17495d17c29af96d0
EuroBas-tech <eurobas10@gmail.com>
2025-11-18 02:07:01 +0100

    Add Hungarian SEO metadata for EuroBas

---
+++ resources/lang/hu/Seo.php
@@ -0,0 +1,2 @@
+'meta_title' => 'EuroBas – Európa egységes piactere járművekhez és ingatlanokhoz',
+'meta_description' => 'Az EuroBas Európa ingyenes és egységes piactere járművek, ingatlanok, jachtok, elektronikai cikkek és számos egyéb termék adásvételéhez. Hozd létre fiókodat, tegyél közzé korlátlan számú ingyenes hirdetést, és lépj kapcsolatba vevőkkel és eladókkal egész Európában.',
