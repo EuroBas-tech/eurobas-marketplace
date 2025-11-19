d10d9f77e32981fae966ddc69ef22ee23b0d2f75
MohammedAmineDeveloper <developermohammed1997@gmail.com>
2025-11-19 18:55:33 +0100

    add cache to language

--- app/Http/Controllers/SharedController.php
+++ app/Http/Controllers/SharedController.php
@@ -5,18 +5,21 @@ namespace App\Http\Controllers;
 use App\CPU\Helpers;
 use App\Model\Seller;
 use App\CPU\ImageManager;
-use App\Model\BusinessSetting;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use App\Model\BusinessSetting;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class SharedController extends Controller
 {
     public function lang($local)
     {
         $direction = 'ltr';
+
         $language = BusinessSetting::where('type', 'language')->first();
+
         foreach (json_decode($language['value'], true) as $key => $data) {
             if ($data['code'] == $local) {
                 $direction = isset($data['direction']) ? $data['direction'] : 'ltr';
@@ -26,15 +29,6 @@ class SharedController extends Controller
         $get_previous_page = explode('/', parse_url(url()->previous(), PHP_URL_PATH));
         $user_type = $get_previous_page[1];
 
-        if($user_type == 'seller') {
-            $seller = Seller::find(auth('seller')->id());
-
-            if ($seller) {
-                $seller->prefered_language = $local;
-                $seller->save();
-            }
-
-        }
 
         session()->forget('language_settings');
         Helpers::language_load();
@@ -43,6 +37,10 @@ class SharedController extends Controller
         
         $previousUrl = url()->previous();
         $previousRequest = Request::create($previousUrl);
+
+        Cache::forget('language');
+        Cache::forget('business_setting_language');
+        Cache::flush();
 
         try {
             $previousRoute = app('router')->getRoutes()->match($previousRequest);
