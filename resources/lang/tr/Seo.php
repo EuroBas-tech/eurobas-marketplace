ed0d0b9ded9dc009fbb45d60fdda587c0b7687c3
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 01:44:00 +0100

    Create Seo.php

---
+++ resources/lang/tr/Seo.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'meta_title' => 'EuroBas – Avrupa’nın birleşik araç ve emlak pazarı',
+    'meta_description' => 'EuroBas, araçlar, emlaklar, yatlar, elektronik ürünler ve daha birçok ürünün alım-satımı için Avrupa’nın ücretsiz ve birleşik ilan platformudur. Hesabınızı oluşturun, sınırsız ücretsiz ilan ekleyin ve Avrupa genelindeki alıcı ve satıcılarla doğrudan iletişime geçin.',
+];
