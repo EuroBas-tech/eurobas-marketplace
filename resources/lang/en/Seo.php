c6ccbd6dbeaa9aaabcabce69d83cc85decec3d21
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 01:23:20 +0100

    Create Seo.php

---
+++ resources/lang/en/Seo.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'meta_title' => "EuroBas – Europe’s Unified Marketplace for Vehicles, Real Estate & More",
+    'meta_description' => "EuroBas is Europe’s unified marketplace for buying and selling vehicles, real estate, yachts, electronics, and many other products. Create your free account, post unlimited ads, and connect directly with buyers and sellers across all European countries.",
+];
