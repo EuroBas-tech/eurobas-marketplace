82d0dc8ebabe4b7893f9c4a86362c3a090a4ae73
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 04:03:52 +0100

    Create Seo.php

---
+++ resources/lang/rs/Seo.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'meta_title' => 'EuroBas – Tržište Evrope za Vozila, Nekretnine i Još Mnogo Toga',
+    'meta_description' => 'EuroBas je evropsko tržište za kupovinu i prodaju vozila, nekretnina, elektronike, jahti i mnogo toga — uz potpuno besplatno oglašavanje, neograničen broj oglasa i bez ikakve provizije. Kreirajte besplatan nalog, postavite neograničeno oglasa i povežite se direktno sa kupcima i prodavcima širom Evrope.',
+];
