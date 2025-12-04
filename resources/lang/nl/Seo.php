304c4d23a9089994cd692dba0aed6331e4029664
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 02:27:48 +0100

    Update Seo.php

--- resources/lang/nl/Seo.php
+++ resources/lang/nl/Seo.php
@@ -1,6 +1,4 @@
-<?php
-
 return [
-    'meta_title' => "EuroBas – Europa’s Marktplaats voor Voertuigen, Vastgoed & Meer",
-    'meta_description' => "EuroBas is Europa’s marktplaats voor de aankoop en verkoop van voertuigen, vastgoed, elektronica, jachten en vele andere producten. Maak gratis een account aan, plaats onbeperkte advertenties en kom direct in contact met kopers en verkopers in heel Europa.",
+    'meta_title' => 'EuroBas – Europa’s Marktplaats voor Voertuigen, Vastgoed & Meer',
+    'meta_description' => 'EuroBas is Europa’s marktplaats voor de aan- en verkoop van voertuigen, vastgoed, elektronica, jachten en meer — volledig gratis en zonder commissie. Maak een gratis account aan, plaats onbeperkte advertenties en kom direct in contact met kopers en verkopers in heel Europa.',
 ];
