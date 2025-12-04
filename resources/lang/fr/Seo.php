340721ae8073dc745fb3122bfb0569e3996e2d10
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 03:53:25 +0100

    Update Seo.php

--- resources/lang/fr/Seo.php
+++ resources/lang/fr/Seo.php
@@ -1,3 +1,4 @@
+<?php
 return [
     'meta_title' => 'EuroBas – Le Marché Européen des Véhicules, Immobilier & Plus',
     'meta_description' => 'EuroBas est la place de marché européenne pour acheter et vendre des véhicules, des biens immobiliers, de l’électronique, des yachts et plus encore — entièrement gratuit et sans commission. Créez votre compte gratuit, publiez des annonces illimitées et entrez en contact directement avec des acheteurs et des vendeurs partout en Europe.',
