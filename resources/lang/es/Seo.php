c1ff5547ba3b4be113eb0ec2a8fab20b764ef866
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 01:54:59 +0100

    Create Seo.php

---
+++ resources/lang/es/Seo.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'meta_title' => "EuroBas – El mercado europeo para vehículos, propiedades y más",
+    'meta_description' => "EuroBas es el mercado europeo para comprar y vender vehículos, propiedades, electrónica, yates y muchos otros productos. Crea tu cuenta gratuita, publica anuncios ilimitados y conéctate directamente con compradores y vendedores en toda Europa.",
+];
