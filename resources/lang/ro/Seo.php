fe496d5184907516af29ea1ed2f93eeca1d7b7c7
EuroBas-tech <eurobas10@gmail.com>
2025-12-04 03:55:56 +0100

    Update Seo.php

--- resources/lang/ro/Seo.php
+++ resources/lang/ro/Seo.php
@@ -1,3 +1,4 @@
+<?php
 return [
     'meta_title' => 'EuroBas – Piața Europeană pentru Vehicule, Imobiliare și Mai Mult',
     'meta_description' => 'EuroBas este piața europeană pentru cumpărarea și vânzarea de vehicule, proprietăți, electronice, iahturi și multe altele — complet gratuit și fără comision. Creează-ți contul gratuit, publică anunțuri nelimitate și intră în contact direct cu cumpărători și vânzători din toată Europa.',
