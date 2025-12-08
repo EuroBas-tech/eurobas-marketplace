40bc063bdc4cdf7200cc9be628345ba7c35a3ba0
MohammedAmineDeveloper <developermohammed1997@gmail.com>
2025-12-08 21:48:54 +0100

    -

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -187,7 +187,7 @@ class HomeController extends Controller
         }
     }
 
-    
+
 
 
 }
--- resources/themes/theme_aster/theme-views/layouts/adding-app.blade.php
+++ resources/themes/theme_aster/theme-views/layouts/adding-app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ session()->get('direction') }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ LaravelLocalization::getCurrentLocale() == 'ar' ? 'rtl' : 'ltr' }}">
 <head>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
