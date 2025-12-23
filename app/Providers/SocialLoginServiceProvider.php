35f77d239c3f91780a9521c6e58373293abe5413
EuroBas-tech <eurobas10@gmail.com>
2025-12-23 18:58:32 +0100

    Ensure secure URLs for social login redirects
    Updated the redirect URLs for Google and Facebook login to use secure URLs.

--- app/Providers/SocialLoginServiceProvider.php
+++ app/Providers/SocialLoginServiceProvider.php
@@ -23,30 +23,32 @@ class SocialLoginServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        try {
-            $socialLoginServices =  Helpers::get_business_settings('social_login');
+    
+public function boot()
+{
+    try {
+        $socialLoginServices = Helpers::get_business_settings('social_login');
 
-            if ($socialLoginServices) {
-                foreach ($socialLoginServices as $socialLoginService) {
-                    if ($socialLoginService['status'] == true && $socialLoginService['login_medium'] == 'google') {
-                        $google_config = array(
-                            'client_id' => $socialLoginService['client_id'],
-                            'client_secret' => $socialLoginService['client_secret'],
-                            'redirect' => url('customer/auth/login/google/callback'),
-                        );
-                        Config::set('services.google', $google_config);
-                    } elseif ($socialLoginService['status'] == true && $socialLoginService['login_medium'] == 'facebook') {
-                        $facebook_config = array(
-                            'client_id' => $socialLoginService['client_id'],
-                            'client_secret' => $socialLoginService['client_secret'],
-                            'redirect' => url('customer/auth/login/facebook/callback'),
-                        );
-                        Config::set('services.facebook', $facebook_config);
-                    }
+        if ($socialLoginServices) {
+            foreach ($socialLoginServices as $socialLoginService) {
+                if ($socialLoginService['status'] == true && $socialLoginService['login_medium'] == 'google') {
+                    $google_config = array(
+                        'client_id' => $socialLoginService['client_id'],
+                        'client_secret' => $socialLoginService['client_secret'],
+                        // تم التعديل هنا لضمان استخدام HTTPS دائماً مع جوجل
+                        'redirect' => secure_url('customer/auth/login/google/callback'),
+                    );
+                    Config::set('services.google', $google_config);
+                } elseif ($socialLoginService['status'] == true && $socialLoginService['login_medium'] == 'facebook') {
+                    $facebook_config = array(
+                        'client_id' => $socialLoginService['client_id'],
+                        'client_secret' => $socialLoginService['client_secret'],
+                        // تم التعديل هنا لفيسبوك أيضاً مع الرابط الخاص به
+                        'redirect' => secure_url('customer/auth/login/facebook/callback'),
+                    );
+                    Config::set('services.facebook', $facebook_config);
                 }
             }
-        }catch(\Exception $exception){}
-    }
+        }
+    } catch (\Exception $exception) {}
 }
