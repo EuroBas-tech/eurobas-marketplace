48c97701371e50b0643fe5cfa371f7460c8f9948
MohammedAmineDeveloper <developermohammed1997@gmail.com>
2026-01-05 17:01:10 +0100

    =

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('PromotionalVideos:AutoDelete')->everyMinute();
+        $schedule->command('PromotionalVideos:AutoDelete')->everyTwoHours();
     }
 
     /**
