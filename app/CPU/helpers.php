8133518db7fb06da814868e956f243229a62a8df
MohammedAmineDeveloper <developermohammed1997@gmail.com>
2026-01-30 15:34:59 +0100

    correcting user_id value from customer to user

--- app/CPU/helpers.php
+++ app/CPU/helpers.php
@@ -1056,7 +1056,7 @@ class Helpers
 
     public static function trackUserCategoryInterest(int $categoryId, int $points = 1): void
     {
-        $userId  = auth('customer')->id();
+        $userId  = auth('customer')->user()->id;
         $guestId = session()->getId();
 
         Log::debug($userId);
@@ -1075,7 +1075,6 @@ class Helpers
     }
 
 }
-
 
 if (!function_exists('currency_symbol')) {
     function currency_symbol()
