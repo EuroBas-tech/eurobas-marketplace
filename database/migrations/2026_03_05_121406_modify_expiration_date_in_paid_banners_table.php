7d4b698fb925c805dcaa29ddc2b83f4c968d9ab3
DeveloperMohammedAmine <developermohammed1997@gmail.com>
2026-03-05 15:11:18 +0300

    =

--- database/migrations/2026_03_05_121406_modify_expiration_date_in_paid_banners_table.php
+++ database/migrations/2026_03_05_121406_modify_expiration_date_in_paid_banners_table.php
@@ -3,15 +3,17 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class ModifyExpirationDateInPaidBannersTable extends Migration
 {
 
-    public function up()
+    public function up(): void
     {
-        Schema::table('paid_banners', function (Blueprint $table) {
-            $table->timestamp('expiration_date')->nullable()->default(null)->change();
-        });
+        DB::statement("
+            ALTER TABLE paid_banners 
+            MODIFY expiration_date TIMESTAMP NULL DEFAULT NULL
+        ");
     }
 
     public function down()
