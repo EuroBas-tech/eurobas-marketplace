a52f3b5f7b718096a5b9b7241eb2a4f201f5116c
DeveloperMohammedAmine <developermohammed1997@gmail.com>
2026-03-05 15:52:43 +0300

    =

--- resources/themes/theme_aster/file_names.php
+++ resources/themes/theme_aster/file_names.php
@@ -4,7 +4,7 @@ return [
     'home' => 'theme-views.home',
     'product_search_result' => 'theme-views.partials._search-result',
     'privacy_policy_page' => 'theme-views.pages.privacy-policy',
-    'instructions_for_use' => 'theme-views.pages.instructions-for-use',
+    'instructions_for_use_page' => 'theme-views.pages.instructions-for-use',
     'return_policy_page' => 'theme-views.pages.return-policy',
     'terms_conditions_page' => 'theme-views.pages.terms-conditions',
     'cancellation_policy_page' => 'theme-views.pages.cancellation-policy',
