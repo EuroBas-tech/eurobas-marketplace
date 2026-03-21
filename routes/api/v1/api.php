8808253fcd2f58f351fba58a5de8b24829e1cf1b
DeveloperMohammedAmine <developermohammed1997@gmail.com>
2026-03-21 18:23:47 +0300

    cleaning controllers and just keeping the newest api endpoints

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -46,17 +46,6 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
         Route::get('/', 'ConfigController@configuration');
     });
 
-    Route::get('common-track-order', 'OrderController@common_track_order');
-
-    Route::group(['prefix' => 'shipping-method','middleware'=>'apiGuestCheck'], function () {
-        Route::get('detail/{id}', 'ShippingMethodController@get_shipping_method_info');
-        Route::get('by-seller/{id}/{seller_is}', 'ShippingMethodController@shipping_methods_by_seller');
-        Route::post('choose-for-order', 'ShippingMethodController@choose_for_order');
-        Route::get('chosen', 'ShippingMethodController@chosen_shipping_methods');
-
-        Route::get('check-shipping-type','ShippingMethodController@check_shipping_type');
-    });
-
      Route::group(['prefix' => 'cart','middleware'=>'apiGuestCheck'], function () {
         Route::get('/', 'CartController@cart');
         Route::post('add', 'CartController@add_to_cart');
@@ -78,21 +67,6 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
         Route::get('products/{brand_id}', 'BrandController@get_products');
     });
 
-    Route::group(['prefix' => 'attributes'], function () {
-        Route::get('/', 'AttributeController@get_attributes');
-    });
-
-    Route::group(['prefix' => 'flash-deals'], function () {
-        Route::get('/', 'FlashDealController@get_flash_deal');
-        Route::get('products/{deal_id}', 'FlashDealController@get_products');
-    });
-
-    Route::group(['prefix' => 'deals'], function () {
-        Route::get('featured', 'DealController@get_featured_deal');
-    });
-
-    Route::get('deal-of-the-day', 'DealOfTheDayController@get_deal_of_the_day_product');
-
     Route::group(['prefix' => 'categories'], function () {
         Route::get('/', 'CategoryController@get_categories');
     });
@@ -101,33 +75,7 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
         Route::get('/', 'PaidBannerController@get_paid_banners');
     });
 
-    Route::group(['prefix' => 'products'], function () {
-        Route::get('reviews/{product_id}', 'ProductController@get_product_reviews');
-        Route::get('rating/{product_id}', 'ProductController@get_product_rating');
-        Route::get('counter/{product_id}', 'ProductController@counter');
-        Route::get('shipping-methods', 'ProductController@get_shipping_methods');
-        Route::get('social-share-link/{product_id}', 'ProductController@social_share_link');
-        Route::post('reviews/submit', 'ProductController@submit_product_review')->middleware('auth:api');
-    });
-
     Route::group(['middleware' => 'apiGuestCheck'], function () {
-        Route::group(['prefix' => 'products'], function () {
-            Route::get('latest', 'ProductController@get_latest_products');
-            Route::get('featured', 'ProductController@get_featured_products');
-            Route::get('top-rated', 'ProductController@get_top_rated_products');
-            Route::any('search', 'ProductController@get_searched_products');
-            Route::post('product-filter', 'ProductController@product_filter');
-            Route::get('details/{slug}', 'ProductController@get_product');
-            Route::get('related-products/{product_id}', 'ProductController@get_related_products');
-            Route::get('best-sellings', 'ProductController@get_best_sellings');
-            Route::get('home-categories', 'ProductController@get_home_categories');
-            ROute::get('discounted-product', 'ProductController@get_discounted_product');
-            Route::get('most-demanded-product', 'ProductController@get_most_demanded_product');
-        });
-
-        Route::group(['prefix' => 'seller'], function () {
-            Route::get('{seller_id}/products', 'SellerController@get_seller_products');
-        });
 
         Route::group(['prefix' => 'categories'], function () {
             Route::get('products/{category_id}', 'CategoryController@get_products');
@@ -149,13 +97,6 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
                 Route::get('list', 'CustomerController@address_list');
             });
 
-            Route::group(['prefix' => 'order'], function () {
-                Route::get('place', 'OrderController@place_order');
-                Route::get('offline-payment-method-list', 'OrderController@offline_payment_method_list');
-                Route::post('place-by-offline-payment', 'OrderController@place_order_by_offline_payment');
-                Route::get('details', 'CustomerController@get_order_details');
-                Route::post('again', 'OrderController@order_again');
-            });
         });
     });
 
@@ -185,32 +126,10 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
             Route::post('close', 'CustomerController@support_ticket_close');
         });
 
-        Route::group(['prefix' => 'compare'], function () {
-            Route::get('list', 'CompareController@list');
-            Route::post('product-store', 'CompareController@compare_product_store');
-            Route::delete('clear-all', 'CompareController@clear_all');
-        });
-
         Route::group(['prefix' => 'wish-list'], function () {
             Route::get('/', 'CustomerController@wish_list');
             Route::post('add', 'CustomerController@add_to_wishlist');
             Route::delete('remove', 'CustomerController@remove_from_wishlist');
-        });
-
-        Route::group(['prefix' => 'order'], function () {
-            Route::get('list', 'CustomerController@get_order_list');
-            Route::get('get-order-by-id', 'CustomerController@get_order_by_id');
-            Route::get('place-by-wallet', 'OrderController@place_order_by_wallet');
-            Route::get('refund', 'OrderController@refund_request');
-            Route::post('refund-store', 'OrderController@store_refund');
-            Route::get('refund-details', 'OrderController@refund_details');
-            Route::post('deliveryman-reviews/submit', 'ProductController@submit_deliveryman_review');
-            Route::get('digital-product-download/{id}', 'OrderController@digital_product_download');
-            Route::get('generate-invoice/{id}', 'OrderController@generate_invoice');
-        });
-
-        Route::group(['prefix' => 'deliveryman'], function () {
-            Route::get('info/{id}', 'DeliveryManController@deliveryman_info');
         });
 
         // Chatting
@@ -220,18 +139,6 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
             Route::post('send-message', 'ChatController@send_message');
         });
 
-        //wallet
-        Route::group(['prefix' => 'wallet'], function () {
-            Route::get('list', 'UserWalletController@list');
-        });
-        //loyalty
-        Route::group(['prefix' => 'loyalty'], function () {
-            Route::get('list', 'UserLoyaltyController@list');
-            Route::post('loyalty-exchange-currency', 'UserLoyaltyController@loyalty_exchange_currency');
-        });
-
-        //seller
-        Route::get('recent-ordered-shops', 'SellerController@get_recent_ordered_shops');
     });
 
     Route::get('ads/show/{ad}', 'AdController@show')->name('show-ad');
@@ -249,27 +156,10 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1', 'middleware' => ['api_l
         Route::post('/', [PaymentController::class, 'customer_add_to_fund_request']);
     });
 
-    Route::group(['prefix' => 'order'], function () {
-        Route::get('track', 'OrderController@track_order');
-        Route::get('cancel-order','OrderController@order_cancel');
-    });
-
     Route::group(['prefix' => 'banners'], function () {
         Route::get('/', 'BannerController@get_banners');
     });
 
-    Route::group(['prefix' => 'seller'], function () {
-        Route::get('/', 'SellerController@get_seller_info');
-        Route::get('{seller_id}/all-products', 'SellerController@get_seller_all_products');
-        Route::get('top', 'SellerController@get_top_sellers');
-        Route::get('all', 'SellerController@get_all_sellers');
-    });
-
-    Route::group(['prefix' => 'coupon','middleware' => 'auth:api'], function () {
-        Route::get('apply', 'CouponController@apply');
-    });
-    
-    Route::get('coupon/list', 'CouponController@list');
     Route::get('get-guest-id', 'GeneralController@get_guest_id');
 
     //map api
