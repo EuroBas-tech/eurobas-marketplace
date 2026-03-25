a53595e1ad4940fe1cdec9f30f38e9acfa8e239d
EuroBas-tech <eurobas10@gmail.com>
2026-03-25 14:02:51 +0100

    Refine auto_builder_sh.sh commands
    Updated composer and artisan commands for better performance and reliability.

--- auto_builder_sh.sh
+++ auto_builder_sh.sh
@@ -1,14 +1,14 @@
- rm -rf package
+rm -rf package
 rm storage/install.zip
 rm storage/update.zip
 rsync -av --exclude '.env' --exclude 'auto_builder.sh' --exclude 'vendor/' --exclude '.git/' --exclude '.idea/' ./ ./package
 
 cd package
-composer update
+composer update --no-interaction --prefer-dist
 cp .env.example .env
 php artisan key:generate
 php artisan passport:keys --skip-if-exists
-php artisan passport:install --force
+php artisan passport:install --copy --force
 
 mkdir -p storage/framework/{sessions,views,cache}
 chmod -R 775 storage/framework
