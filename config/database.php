9f20a33669ba211020df7930cc1361b794782e2f
EuroBas-tech <eurobas10@gmail.com>
2026-05-11 03:54:19 +0200

    Change Redis cluster environment variable to 'redis'

--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
 
         'options' => [
               
-            'cluster' => env('REDIS_CLUSTER', 'atomic'), 
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'eurobas'), '_').'_database_'),
         ],
 
