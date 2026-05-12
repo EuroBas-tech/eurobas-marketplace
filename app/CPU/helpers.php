ad55ad415b981c5a744003b3a81f69222bca979f
EuroBas-tech <eurobas10@gmail.com>
2026-05-12 06:10:49 +0200

    Refactor cache retrieval for translations

--- app/CPU/helpers.php
+++ app/CPU/helpers.php
@@ -1119,11 +1119,11 @@ function translate($key)
         $cacheKey = "translations_{$locale}";
         
         // Get all translations for locale from cache, or load from DB 
-      $translations = Cache::store('file')->rememberForever($cacheKey, function () use ($locale) {
-      return LanguageTranslation::where('locale', $locale)
-      ->pluck('value', 'key')
-      ->toArray();
-    });
+         $translations = Cache::rememberForever($cacheKey, function () use ($locale) {
+            return LanguageTranslation::where('locale', $locale)
+            ->pluck('value', 'key')
+            ->toArray();
+        });
 
         $processedKey = ucfirst(str_replace('_', ' ', Helpers::remove_invalid_charcaters($key)));
         $key = Helpers::remove_invalid_charcaters($key);
