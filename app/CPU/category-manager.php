090397f5409a05cf58eb55bd07d8f1852f1ca7b9
EuroBas-tech <eurobas10@gmail.com>
2026-05-12 19:53:37 +0200

    Implement caching in category manager methods
    Refactor category retrieval methods to use caching for improved performance.

--- app/CPU/category-manager.php
+++ app/CPU/category-manager.php
@@ -4,19 +4,34 @@ namespace App\CPU;
 
 use App\Model\Category;
 use App\Model\Product;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryManager
 {
     public static function parents()
     {
-        $x = Category::with(['childes.childes'])->where('position', 0)->priority()->get();
-        return $x;
+        $locale = app()->getLocale();
+        
+        return Cache::remember('categories_parents_' . $locale, now()->addDays(7), function () use ($locale) {
+            return Category::with(['childes.childes', 'translations' => function($query) use ($locale) {
+                $query->where('locale', $locale);
+            }])
+            ->where('position', 0)
+            ->priority()
+            ->get();
+        });
     }
 
     public static function child($parent_id)
     {
-        $x = Category::where(['parent_id' => $parent_id])->get();
-        return $x;
+        $locale = app()->getLocale();
+        return Cache::remember('categories_child_' . $parent_id . '_' . $locale, now()->addDays(7), function () use ($parent_id, $locale) {
+            return Category::with(['translations' => function($query) use ($locale) {
+                $query->where('locale', $locale);
+            }])
+            ->where(['parent_id' => $parent_id])
+            ->get();
+        });
     }
 
     public static function products($category_id, $request=null)
@@ -32,26 +47,38 @@ class CategoryManager
     }
 
     public static function get_category_name($id){
-        $category = Category::find($id);
+        $locale = app()->getLocale();
+        return Cache::remember('category_name_' . $id . '_' . $locale, now()->addDays(7), function () use ($id, $locale) {
+            $category = Category::with(['translations' => function($query) use ($locale) {
+                $query->where('locale', $locale);
+            }])->find($id);
 
-        if($category){
-            return $category->name;
-        }
-        return '';
+            if($category){
+                return $category->name;
+            }
+            return '';
+        });
     }
 
     public static function get_categories_with_counting()
     {
-        $categories = Category::withCount(['product'=>function($query){
-                        $query->where(['status'=>'1']);
-                    }])->with(['childes' => function ($query) {
-                        $query->with(['childes' => function ($query) {
-                            $query->withCount(['sub_sub_category_product'])->where('position', 2);
-                        }])->withCount(['sub_category_product'])->where('position', 1);
-                    }, 'childes.childes'])
-                    ->where('position', 0)
-                    ->get();
-
-        return $categories;
+        $locale = app()->getLocale();
+        return Cache::remember('categories_counting_' . $locale, now()->addDays(7), function () use ($locale) {
+            return Category::withCount(['product'=>function($query){
+                            $query->where(['status'=>'1']);
+                        }])->with(['translations' => function($query) use ($locale) {
+                            $query->where('locale', $locale);
+                        }, 'childes' => function ($query) use ($locale) {
+                            $query->with(['translations' => function($q) use ($locale) {
+                                $q->where('locale', $locale);
+                            }, 'childes' => function ($query) use ($locale) {
+                                $query->with(['translations' => function($q) use ($locale) {
+                                    $q->where('locale', $locale);
+                                }])->withCount(['sub_sub_category_product'])->where('position', 2);
+                            }])->withCount(['sub_category_product'])->where('position', 1);
+                        }])
+                        ->where('position', 0)
+                        ->get();
+        });
     }
 }
