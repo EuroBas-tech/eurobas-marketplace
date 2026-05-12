dd7364cc7fd0c895fe4acc089ffc8b832037d29b
EuroBas-tech <eurobas10@gmail.com>
2026-05-12 20:55:30 +0200

    Refactor category-manager methods for consistency

--- app/CPU/category-manager.php
+++ app/CPU/category-manager.php
@@ -11,12 +11,11 @@ class CategoryManager
     public static function parents()
     {
         $locale = app()->getLocale();
-        
         return Cache::remember('categories_parents_' . $locale, now()->addDays(7), function () use ($locale) {
             return Category::with(['childes.childes', 'translations' => function($query) use ($locale) {
                 $query->where('locale', $locale);
             }])
-            ->where('position', 1)    
+            ->where('position', 1)
             ->priority()
             ->get();
         });
@@ -34,26 +33,27 @@ class CategoryManager
         });
     }
 
-    public static function products($category_id, $request=null)
+    public static function products($category_id, $request = null)
     {
         $user = Helpers::get_customer($request);
-        $id = '"'.$category_id.'"';
-        return Product::with(['rating','tags','seller.shop'])
-            ->withCount(['wish_list' => function($query) use($user){
+        $id = '"' . $category_id . '"';
+        return Product::with(['rating', 'tags', 'seller.shop'])
+            ->withCount(['wish_list' => function ($query) use ($user) {
                 $query->where('customer_id', $user != 'offline' ? $user->id : '0');
             }])
             ->active()
             ->where('category_ids', 'like', "%{$id}%")->get();
     }
 
-    public static function get_category_name($id){
+    public static function get_category_name($id)
+    {
         $locale = app()->getLocale();
         return Cache::remember('category_name_' . $id . '_' . $locale, now()->addDays(7), function () use ($id, $locale) {
-            $category = Category::with(['translations' => function($query) use ($locale) {
+            $category = Category::with(['translations' => function ($query) use ($locale) {
                 $query->where('locale', $locale);
             }])->find($id);
 
-            if($category){
+            if ($category) {
                 return $category->name;
             }
             return '';
@@ -64,21 +64,21 @@ class CategoryManager
     {
         $locale = app()->getLocale();
         return Cache::remember('categories_counting_' . $locale, now()->addDays(7), function () use ($locale) {
-            return Category::withCount(['product'=>function($query){
-                            $query->where(['status'=>'1']);
-                        }])->with(['translations' => function($query) use ($locale) {
-                            $query->where('locale', $locale);
-                        }, 'childes' => function ($query) use ($locale) {
-                            $query->with(['translations' => function($q) use ($locale) {
-                                $q->where('locale', $locale);
-                            }, 'childes' => function ($query) use ($locale) {
-                                $query->with(['translations' => function($q) use ($locale) {
-                                    $q->where('locale', $locale);
-                                }])->withCount(['sub_sub_category_product'])->where('position', 3);    
-                            }])->withCount(['sub_category_product'])->where('position', 2);   
-                        }])
-                        ->where('position', 1)    
-                        ->get();
+            return Category::withCount(['product' => function ($query) {
+                $query->where(['status' => '1']);
+            }])->with(['translations' => function ($query) use ($locale) {
+                $query->where('locale', $locale);
+            }, 'childes' => function ($query) use ($locale) {
+                $query->with(['translations' => function ($q) use ($locale) {
+                    $q->where('locale', $locale);
+                }, 'childes' => function ($query) use ($locale) {
+                    $query->with(['translations' => function ($q) use ($locale) {
+                        $q->where('locale', $locale);
+                    }])->withCount(['sub_sub_category_product'])->where('position', 3);
+                }])->withCount(['sub_category_product'])->where('position', 2);
+            }])
+            ->where('position', 1)
+            ->get();
         });
     }
 }
