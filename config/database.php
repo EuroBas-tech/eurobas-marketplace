bb3c15d82341332e4b40945a3bd592baeaebbf4f
EuroBas-tech <eurobas10@gmail.com>
2026-05-12 06:43:06 +0200

    Update Redis prefix to an empty string
    Change Redis prefix to an empty string in database config.

--- config/database.php
+++ config/database.php
@@ -124,7 +124,7 @@ return [
         'options' => [
               
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'eurobas'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', ''),
         ],
 
         'default' => [
