37e78449750de2f0133db2e7a5194a16bba28c6f
EuroBas-tech <eurobas10@gmail.com>
2026-05-21 02:23:40 +0200

    Refactor user interests retrieval logic

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -66,24 +66,24 @@ $home_categories = Cache::rememberForever('categories_' . $locale, function () u
         $banner = $banners->firstWhere('lang', $locale)
         ?? $banners->firstWhere('lang', 'Both');
 
-         $customerId = auth('customer')->id();
-         $guestId    = Helpers::deviceId();
-         $cacheKey   = 'user_interests_' . ($customerId ?? $guestId);
+          $customerId = auth('customer')->id();
+          $guestId    = Helpers::deviceId();
+          $cacheKey   = 'user_interests_' . ($customerId ?? $guestId);
 
-           $userInterests = Cache::remember($cacheKey, now()->addDay(), function () use ($customerId, $guestId) {
-            return \App\Models\UserCategoryInterest::query()
-             ->select('category_id', 'score')
-              ->where(function ($query) use ($customerId, $guestId) {
-               if ($customerId) {
+         $userInterests = Cache::remember($cacheKey, now()->addDay(), function () use ($customerId, $guestId) {
+          return UserCategoryInterest::query()
+         ->select('category_id', 'score')
+         ->where(function ($query) use ($customerId, $guestId) {
+            if ($customerId) {
                 $query->where('user_id', $customerId);
-                 } else {
-                 $query->where('guest_id', $guestId);
+              } else {
+                $query->where('guest_id', $guestId);
             }
         })
         ->get()
         ->pluck('score', 'category_id')
         ->toArray();
-       });
+      });
 
         $favCategoryId = $userInterests
         ->where(
