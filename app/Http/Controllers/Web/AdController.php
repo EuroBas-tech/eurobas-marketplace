4ff7fe9fcae0b77ce6cf7bda3f35eed4873d453d
EuroBas-tech <eurobas10@gmail.com>
2026-05-29 00:02:36 +0200

    Change cache duration from 1 day to 2 hours

--- app/Http/Controllers/Web/AdController.php
+++ app/Http/Controllers/Web/AdController.php
@@ -773,7 +773,7 @@ class AdController extends Controller
              $guestId    = Helpers::deviceId();
              $cacheKey   = 'user_interests_' . ($customerId ?? $guestId);
 
-             $userInterests = Cache::remember($cacheKey, now()->addDay(), function () use ($customerId, $guestId) {
+              $userInterests = Cache::remember($cacheKey, now()->addHours(2), function () use ($customerId, $guestId) {
               return UserCategoryInterest::query()
               ->where(function ($query) use ($customerId, $guestId) {
                 if ($customerId) {
@@ -1148,7 +1148,7 @@ class AdController extends Controller
          $guestId    = Helpers::deviceId();
          $cacheKey   = 'user_interests_' . ($customerId ?? $guestId);
 
-         $userInterests = Cache::remember($cacheKey, now()->addDay(), function () use ($customerId, $guestId) {
+        $userInterests = Cache::remember($cacheKey, now()->addHours(2), function () use ($customerId, $guestId) {
           return UserCategoryInterest::query()
         ->where(function ($query) use ($customerId, $guestId) {
             if ($customerId) {
