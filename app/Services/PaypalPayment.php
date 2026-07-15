3b53236661637ed8f99c75303d2eae815ce6b6dd
EuroBas-tech <eurobas10@gmail.com>
2026-07-15 05:01:32 +0200

    Change order_id to null in wallet actions
    Set order_id to null when inserting into admin_wallet_actions.

--- app/Services/PaypalPayment.php
+++ app/Services/PaypalPayment.php
@@ -257,7 +257,7 @@ class PaypalPayment
 
             \Illuminate\Support\Facades\DB::table('admin_wallet_actions')->insert([
                 'admin_wallet_id'     => $walletId,
-                'order_id'            => $model->id,
+                'order_id'            => null,
                 'gateway'             => $gateway,
                 'transaction_id'      => $transactionId,
                 'package_type'        => $packageType,
