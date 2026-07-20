c57b9a5d897b14a7f34e6f5a338521b865c2930e
EuroBas-tech <eurobas10@gmail.com>
2026-07-20 17:39:08 +0200

    Update ad link to use LaravelLocalization

--- resources/themes/theme_aster/theme-views/partials/_search-result.blade.php
+++ resources/themes/theme_aster/theme-views/partials/_search-result.blade.php
@@ -70,7 +70,7 @@
     @if(!empty($ads))
         @foreach($ads as $ad)
             <li class="list-group-item p-2">
-                <a class="font-size-16 d-flex align-items-center gap-2" href="{{ url(app()->getLocale() . '/ads/show/' . $ad->slug) }}">
+             <a class="font-size-16 d-flex align-items-center gap-2" href="{{ LaravelLocalization::getLocalizedURL(session('local') ?? app()->getLocale(), route('ads-show', ['slug' => $ad->slug])) }}">
                     <div>
                         <img class="rounded search-image"
                         src="{{ cloudfront('ad/thumbnail/'.$ad->thumbnail) }}" alt="ad_image">
