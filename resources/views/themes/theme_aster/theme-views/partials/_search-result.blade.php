eab79a570218690be167cc9a029a1e5796a18d80
EuroBas-tech <eurobas10@gmail.com>
2026-07-20 04:21:48 +0200

    Replace ads display with products in search results

--- resources/views/themes/theme_aster/theme-views/partials/_search-result.blade.php
+++ resources/views/themes/theme_aster/theme-views/partials/_search-result.blade.php
@@ -1,15 +1,9 @@
-<ul class="list-group list-group-flush">
-    @if(isset($ads) && count($ads) > 0)
-        @foreach($ads as $ad)
-            <li class="list-group-item">
-                <a href="{{ url(app()->getLocale() . '/ads/show/' . $ad->slug) }}">
-                    {{ $ad['title'] }}
-                </a>
-            </li>
-        @endforeach
-    @else
-        <li class="list-group-item text-center">
-            {{ translate('not_found_anything') }}
+<ul class="list-group list-group-flush " >
+    @foreach($products as $product)
+        <li class="list-group-item">
+            <a href="{{route('product',$product->slug)}}" >
+                {{$product['name']}}
+            </a>
         </li>
-    @endif
+    @endforeach
 </ul>
