f673fa7f3b46b36a5c807efd5a857b7a31abaad3
EuroBas-tech <eurobas10@gmail.com>
2026-07-20 20:24:27 +0200

    Add locale prefix to searched ads URL

--- resources/themes/theme_aster/public/assets/js/custom.js
+++ resources/themes/theme_aster/public/assets/js/custom.js
@@ -578,9 +578,12 @@ $(document).on('keyup', '.search-bar-input-mobile', function() {
         let title = $input.val();
         let base_url = $('meta[name="base-url"]').attr("content");
         
-       if (title.length > 0) {
+        if (title.length > 0) {
+            let currentLang = getCurrentLanguage();
+            let localePrefix = (currentLang === 'en') ? '' : '/' + currentLang;
             $.get({
-                url: base_url + "/searched-ads",
+                url: base_url + localePrefix + "/searched-ads",
+                
                 dataType: "json",
                 data: { title },
                 beforeSend: function () {
