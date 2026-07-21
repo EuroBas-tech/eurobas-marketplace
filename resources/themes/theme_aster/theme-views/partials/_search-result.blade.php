72da0ee00cedf6986c61460ddf6a4fa034e413ff
EuroBas-tech <eurobas10@gmail.com>
2026-07-21 05:40:14 +0200

    Refactor ad link generation for locale handling

--- resources/themes/theme_aster/theme-views/partials/_search-result.blade.php
+++ resources/themes/theme_aster/theme-views/partials/_search-result.blade.php
@@ -70,7 +70,7 @@
     @if(!empty($ads))
         @foreach($ads as $ad)
             <li class="list-group-item p-2">
-                <a class="font-size-16 d-flex align-items-center gap-2" href="{{ app()->getLocale() === config('laravellocalization.defaultLocale', 'en') ? route('ads-show', $ad->slug) : url(app()->getLocale() . '/ads/show/' . $ad->slug) }}">
+            <a class="font-size-16 d-flex align-items-center gap-2" href="{{ isset($locale) && $locale !== 'en' ? url($locale . '/ads/show/' . $ad->slug) : route('ads-show', $ad->slug) }}">
                     <div>
                         <img class="rounded search-image"
                         src="{{ cloudfront('ad/thumbnail/'.$ad->thumbnail) }}" alt="ad_image">
