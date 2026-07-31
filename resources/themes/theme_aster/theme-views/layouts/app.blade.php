7de7622b9428067bdb638853b0227cb1cbcc7233
EuroBas-tech <eurobas10@gmail.com>
2026-07-31 16:43:29 +0200

    Update confirmation alert title for localization

--- resources/themes/theme_aster/theme-views/layouts/app.blade.php
+++ resources/themes/theme_aster/theme-views/layouts/app.blade.php
@@ -353,7 +353,7 @@
 <script>
     function route_alert(route, message) {
         Swal.fire({
-            title: '{{translate("are_you_sure")}}?',
+            title: '{{translate("are_you_sure")}}{{ app()->getLocale() == "ar" ? "؟" : "?" }}',
             text: message,
             type: 'warning',
             showCancelButton: true,
