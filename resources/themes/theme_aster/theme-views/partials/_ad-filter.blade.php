e5003d426d7a5ff507166293e691fea49f994653
EuroBas-tech <eurobas10@gmail.com>
2026-08-03 01:27:54 +0200

    Update IDs for brand and model select elements

--- resources/themes/theme_aster/theme-views/partials/_ad-filter.blade.php
+++ resources/themes/theme_aster/theme-views/partials/_ad-filter.blade.php
@@ -183,7 +183,7 @@
                                                         <div class="col-xl-4 col-md-4 col-sm-6 col-6 px-1 input-responsive-height mt-1" data-category-type="vehicles"
                                                         data-for="cars, trucks, classic-cars, supercars, buses, spare-parts, motorcycles, caravans, heavy-equipment, agricultural-machinery, vehicle-accessories, agricultural-machinery">
                                                             <div class="form-group">
-                                                                <select class="form-control filter-input input-responsive-height font-size-16 brand-select" name="brand_id" id="brand">
+                                                                <select class="form-control filter-input input-responsive-height font-size-16 brand-select" name="brand_id" id="brand-desktop">
                                                                     <option value="all">{{translate('brand')}}</option>
                                                                     @foreach($brands as $brand)
                                                                         <option
@@ -200,7 +200,7 @@
                                                         <div class="col-xl-4 col-md-4 col-sm-6 col-6 px-1 input-responsive-height mt-1"
                                                         data-category-type="vehicles" data-for="cars, trucks, classic-cars, supercars, spare-parts, motorcycles, buses, motorcycle-parts, caravans, heavy-equipment, agricultural-machinery, agricultural-machinery">
                                                             <div class="form-group">
-                                                                <select disabled class="form-control filter-input input-responsive-height font-size-16 model-select" name="model_id" id="model">
+                                                                <select disabled class="form-control filter-input input-responsive-height font-size-16 model-select" name="model_id" id="model-desktop">
                                                                     <option value="all">{{translate('model')}}</option>
                                                                     @foreach($models as $model)
                                                                         <option data-brand-id="{{ $model['brand_id'] }}"
@@ -752,11 +752,11 @@
 
 <script>
     $(document).ready(function () {
-        const $brandSelect = $('#brand');
-        const $modelSelect = $('#model');
+        const $brandSelect = $('#brand-desktop');
+        const $modelSelect = $('#model-desktop');
         const $categoryInput = $('#selectedCategoryId');
 
-        $('#model').prop('disabled', true);
+        $('#model-desktop').prop('disabled', true);
 
         // Initialize Select2
         $brandSelect.select2({
@@ -849,7 +849,7 @@
                 if (selectedValue === 'all') {
                     $modelSelect.val('all').trigger('change').prop('disabled', true);
                 } else {
-                    $('#model').prop('disabled', false);
+                    $('#model-desktop').prop('disabled', false);
                 }
             }
         });
@@ -863,4 +863,3 @@
 
     });
 </script>
-
