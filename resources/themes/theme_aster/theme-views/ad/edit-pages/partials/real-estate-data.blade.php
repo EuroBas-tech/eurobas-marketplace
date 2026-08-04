655f7bb0be8bb1af27f2a26f472b90c331dac709
EuroBas-tech <eurobas10@gmail.com>
2026-08-04 06:07:52 +0200

    Update property size input to include units

--- resources/themes/theme_aster/theme-views/ad/edit-pages/partials/real-estate-data.blade.php
+++ resources/themes/theme_aster/theme-views/ad/edit-pages/partials/real-estate-data.blade.php
@@ -77,8 +77,11 @@
 
     <div class="col-xl-4 mt-2 mt-sm-0">
         <div class="form-group">
-            <label for="property_size">{{translate('property_size')}}</label>
-            <input type="text" class="form-control" value="{{$ad->property_size}}" name="property_size" placeholder="{{translate('property_size')}}">
+            <label for="property_size">{{translate('property_size')}} (m²)</label>
+            <div class="input-group" dir="ltr">
+                <input type="number" min="0" class="form-control" value="{{$ad->property_size}}" name="property_size" placeholder="e.g. 150">
+                <span class="input-group-text">m²</span>
+            </div>
         </div>
     </div>
 
