a060eeb794e7ac632ffbfe7ab18080f3ac6f9641
EuroBas-tech <eurobas10@gmail.com>
2026-08-07 21:10:16 +0200

    Change container class to container-fluid in ad view تعديل عرض الصفحه البدايه

--- resources/themes/theme_aster/theme-views/ad/ad-adding-type.blade.php
+++ resources/themes/theme_aster/theme-views/ad/ad-adding-type.blade.php
@@ -30,7 +30,7 @@
 @section('content')
     <!-- Main Content -->
     <main class="main-content d-flex flex-column gap-3 py-3 mb-4 vh-100">
-        <div class="container">
+       <div class="container-fluid px-2 px-sm-auto">
             <div class="row justify-content-center">
                 <!-- Sidebar-->
                 <div class="col-lg-10">
