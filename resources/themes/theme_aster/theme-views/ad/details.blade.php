ac4908c27b18d472f3dde46cbf01e3f6337bbcac
EuroBas-tech <eurobas10@gmail.com>
2026-08-07 18:33:20 +0200

    Update column classes in ad details view

--- resources/themes/theme_aster/theme-views/ad/details.blade.php
+++ resources/themes/theme_aster/theme-views/ad/details.blade.php
@@ -259,7 +259,7 @@
     <main class="main-content d-flex flex-column gap-3 pt-3 mb-sm-5">
         <div class="container px-0 px-sm-3">
             <div class="row gx-3 gy-4">
-                <div class="col-lg-8 col-xl-8 flex-grow-1 px-sm-3 px-0">
+                <div class="col-12 col-lg-8 col-xl-8 px-sm-3 px-0">
                     <div class="card h-100 mb-3 px-sm-0 px-3">
                         <div class="card-body px-sm-3 px-0">
                             <div class="quickview-content">
