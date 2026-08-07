87a306eb83ca0971b7d749e74b41a1a14e7aee81
EuroBas-tech <eurobas10@gmail.com>
2026-08-07 19:08:14 +0200

    Add responsive margin adjustments for small screens

--- resources/themes/theme_aster/theme-views/layouts/app.blade.php
+++ resources/themes/theme_aster/theme-views/layouts/app.blade.php
@@ -81,6 +81,13 @@
              overflow-x: hidden;
              max-width: 100vw;
              }
+        
+        @media (max-width: 767.98px) {
+            .row {
+                margin-left: 0 !important;
+                margin-right: 0 !important;
+            }
+        }
 
         :root {
             --bs-primary: {{ $web_config['primary_color'] }};
