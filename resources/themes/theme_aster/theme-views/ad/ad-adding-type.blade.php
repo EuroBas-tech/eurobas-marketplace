9f1c302af2deee5371974782b9009fccee9efaba
EuroBas-tech <eurobas10@gmail.com>
2026-08-07 21:27:56 +0200

    Adjust row class for ad-adding-type view

--- resources/themes/theme_aster/theme-views/ad/ad-adding-type.blade.php
+++ resources/themes/theme_aster/theme-views/ad/ad-adding-type.blade.php
@@ -31,7 +31,7 @@
     <!-- Main Content -->
     <main class="main-content d-flex flex-column gap-3 py-3 mb-4 vh-100">
        <div class="container-fluid px-2 px-sm-auto">
-            <div class="row justify-content-center">
+        <div class="row justify-content-center gx-0">
                 <!-- Sidebar-->
                 <div class="col-lg-10">
                     <div class="card h-lg-100 card-custom-shadow">
