593c9b26fb422df40471a0d05f96a502b4d06ac2
EuroBas-tech <eurobas10@gmail.com>
2026-08-08 17:35:11 +0200

    Increase font size in select2 search field

--- resources/themes/theme_aster/theme-views/layouts/adding-app.blade.php
+++ resources/themes/theme_aster/theme-views/layouts/adding-app.blade.php
@@ -95,6 +95,7 @@
         .select2-container--default .select2-search--dropdown .select2-search__field {
             border: 1px solid gray !important;
             border-radius: 4px !important;
+            font-size: 16px !important;
         }
 
 
