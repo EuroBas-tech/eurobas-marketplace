c31a9b20619a3681c16c05f4a6f82d6cb2f53075
EuroBas-tech <eurobas10@gmail.com>
2026-08-23 04:56:51 +0200

    Remove conditional margin-top for main content
    Removed conditional margin-top styling for main content based on customer authentication and user details.

--- resources/themes/theme_aster/theme-views/layouts/home-app.blade.php
+++ resources/themes/theme_aster/theme-views/layouts/home-app.blade.php
@@ -117,12 +117,7 @@
             'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji',
             'Segoe UI Emoji', 'Segoe UI Symbol';
         }
-
-        @if(auth("customer")->check() && (!auth("customer")->user()->phone_code || !auth('customer')->user()->phone || !auth('customer')->user()->country || !auth('customer')->user()->city))
-            .main-content {
-                margin-top: 40px;
-            }
-        @endif
+                   
 
     </style>
     @php($google_tag_manager_id = \App\CPU\Helpers::get_business_settings('google_tag_manager_id'))
