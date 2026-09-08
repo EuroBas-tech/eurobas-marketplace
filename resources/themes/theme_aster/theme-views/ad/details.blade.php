60f63b6b8de491cc5d4e4afbcf1c93ad20e359f6
EuroBas-tech <eurobas10@gmail.com>
2026-09-08 02:07:29 +0200

    Refactor layout structure in details.blade.php

--- resources/themes/theme_aster/theme-views/ad/details.blade.php
+++ resources/themes/theme_aster/theme-views/ad/details.blade.php
@@ -258,9 +258,9 @@
     <!-- Main Content -->
     <main class="main-content d-flex flex-column gap-3 pt-3 mb-sm-5">
         <div class="container px-0 px-sm-3">
-             <div class="row gx-0 gy-4 align-items-start">
+             <div class="row gx-0 gy-4">
                 <div class="col-12 col-lg-8 col-xl-8 px-sm-3 px-0">
-                 <div class="card mb-3 px-sm-0 px-3">
+                 <div class="card h-100 mb-3 px-sm-0 px-3">
                         <div class="card-body px-sm-3 px-0">
                             <div class="quickview-content">
                                 <div class="mb-4" >
@@ -1634,8 +1634,8 @@
                         </div>
                     </div>
                 </div>
-               <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12" style="align-self:flex-start;position:sticky;top:80px;">
-                    <div class="card">
+                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12" >
+                    <div class="card h-100">
                         <div class="card-body">
                             <div class="" >
                                 <div class="d-flex align-items-start justify-content-between" >
