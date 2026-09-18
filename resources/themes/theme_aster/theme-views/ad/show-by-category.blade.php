324e37a0359f31283ef855156065d49c9d21b549
EuroBas-tech <eurobas10@gmail.com>
2026-09-18 02:48:31 +0200

    Update emoji font source URL to jsDelivr

--- resources/themes/theme_aster/theme-views/ad/show-by-category.blade.php
+++ resources/themes/theme_aster/theme-views/ad/show-by-category.blade.php
@@ -16,7 +16,7 @@
         @font-face {
             font-family: 'NotoColorEmojiLimited';
             unicode-range: U+1F1E6-1F1FF;
-            src: url('https://raw.githack.com/googlefonts/noto-emoji/main/fonts/NotoColorEmoji.ttf') format('truetype');
+            src: url('https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/fonts/NotoColorEmoji.ttf') format('truetype');
         }
 
         .emoji-font {
